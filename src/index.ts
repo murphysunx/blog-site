52c00d451a86a7381c63a8c927c535e02b38ff46
Murphy Sun <murphysunx@gmail.com>
2023-02-22 23:09:41 +1100

    #feat: graphql setup

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import mikroOrmConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroOrmConfig);
+  orm.getMigrator().up();
+  // need to use fork() method so avoiding global Entitiy manager error
+  // https://stackoverflow.com/a/72799993
+  const em = orm.em.fork();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em }),
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
+  // const post = em.create(Post, {
+  //   title: "my first post",
+  //   createdAt: "",
+  //   updatedAt: "",
+  // });
+  // await em.persistAndFlush(post);
+
+  // const posts = await em.find(Post, {});
+  // console.log(posts);
+};
+
+main().catch((err) => {
+  console.error(err);
+});
