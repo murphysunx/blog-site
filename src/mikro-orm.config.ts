fddbe80e5ee7a58516e6a9e01921965db2a6b3b6
Murphy Sun <murphysunx@gmail.com>
2023-02-23 22:46:04 +1100

    #feat: register / login

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,17 +1,18 @@
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
-import { MikroORM } from "@mikro-orm/core"
+import { MikroORM } from "@mikro-orm/core";
 import path from "path";
+import { User } from "./entities/User";
 
 export default {
   migrations: {
     path: path.join(__dirname, "./migrations"),
-    pattern: /^[\w-]+\d+\.[tj]s$/
+    pattern: /^[\w-]+\d+\.[tj]s$/,
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: "blogsite",
   type: "postgresql",
   debug: !__prod__,
   user: "postgres",
-  password: "",
+  password: "asd1213474",
 } as Parameters<typeof MikroORM.init>[0];
