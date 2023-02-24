24a00df5a7e56edb10346ea6e7290a54de7a8e94
Murphy Sun <murphysunx@gmail.com>
2023-02-24 23:14:46 +1100

    #feat: session setup

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
+import { ApolloServer } from "apollo-server-express";
+import connectRedis from "connect-redis";
+import express from "express";
+import session from "express-session";
+import { createClient } from "redis";
+import "reflect-metadata";
+import { buildSchema } from "type-graphql";
 import { __prod__ } from "./constants";
 import mikroOrmConfig from "./mikro-orm.config";
-import express from "express";
-import { ApolloServer } from "apollo-server-express";
-import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import { MyContext } from "./types";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
@@ -18,12 +22,35 @@ const main = async () => {
 
   const app = express();
 
+  const RedisStore = connectRedis(session);
+  // TODO: client not working yet
+  const redisClient = createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTTL: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 100, // 10 years
+        httpOnly: true,
+        sameSite: "lax", // csrf
+        secure: __prod__, // cookie only works in https
+      },
+      saveUninitialized: false,
+      secret: "wfwefwfcvw",
+      resave: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em }),
+    context: ({ req, res }): MyContext => ({ em, req, res }),
   });
 
   await apolloServer.start();
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,15 @@
 import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
+import { Request, Response } from "express";
+
+// https://akoskm.com/how-to-use-express-session-with-custom-sessiondata-typescript
+declare module "express-session" {
+  interface SessionData {
+    userID: number;
+  }
+}
 
 export type MyContext = {
   em: EntityManager<IDatabaseDriver<Connection>>;
+  req: Request;
+  res: Response;
 };
